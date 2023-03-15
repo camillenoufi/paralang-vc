added cent normalization method, fixed plot issue

audio_utils.py
@@ -33,8 +33,8 @@ class Vocoder(nn.Module):
       wavs_pred = self.forward(x_tgt.squeeze(1))
       wavs = self.format_wavs(wavs_true, wavs_pred)
       for j,wav in enumerate(wavs):
-          self.writer.add_audio(f'valid/wavs_E{self.epoch}_{i}_true', wav[0,:].unsqueeze(0), epoch, sample_rate=hp.sampling_rate)
-          self.writer.add_audio(f'valid/wavs_E{self.epoch}_{i}_pred', wav[1,:].unsqueeze(0), epoch, sample_rate=hp.sampling_rate)
+          self.writer.add_audio(f'valid/wavs_E{epoch}_{i}_true', wav[0,:].unsqueeze(0), epoch, sample_rate=hp.sampling_rate)
+          self.writer.add_audio(f'valid/wavs_E{epoch}_{i}_pred', wav[1,:].unsqueeze(0), epoch, sample_rate=hp.sampling_rate)
           if valid:
              fn = os.path.join(self.out_path,f'wavs_E{epoch}_v{j}.wav')
           else:

data.py
@@ -1,6 +1,7 @@
 from torch.utils import data
 import torch.nn.functional as F
 import torch
+import torchaudio
 import numpy as np
 import crepe
 import librosa
@@ -20,6 +21,8 @@ class AutoVCDataset(data.Dataset):
         self.spk_embs_root = spk_embs_root
         self.len_crop = len_crop
         self.transform = transform
+        self.min_c = np.log2(50)
+        self.max_c = np.log2(1000)
         # assert jitter % 32 == 0, "Jitter must be divisible by 32"
         # self.jitter_choices = list(range(0, jitter+1, 32))
 
@@ -49,14 +52,14 @@ class AutoVCDataset(data.Dataset):
         pth = self.paths[index]
         spk_id = pth.parent.stem
         if pth.suffix == '.wav':
-          x, fs = librosa.load(pth, sr=hp.sampling_rate, mono=False)
+          x, fs = librosa.load(pth, mono=False) #resampling happens when converting to melspec
           if self.transform: # do augmentations to .wav files directly so they will propogate to mspecs and conditioning vars
             x = self.apply_transforms(x,fs)
           
           # Get source, TEGG, and EGG spectrograms using the different channels in x
-          mspec_src, x_src = get_mspec_from_array(x=x[0, :], input_sr=fs, is_hifigan=True, return_waveform=True) # (N, n_mels)
-          mspec_tegg, x_tegg = get_mspec_from_array(x=x[1, :], input_sr=fs, is_hifigan=True, return_waveform=True) # (N, n_mels)
-          mspec_egg, x_egg = get_mspec_from_array(x=x[2, :], input_sr=fs, is_hifigan=True, return_waveform=True) # (N, n_mels)
+          mspec_src, x_src = self.get_mspec(self, x[0, :], fs) # (N, n_mels)
+          mspec_tegg, _ = self.get_mspec(self, x[1, :], fs) # (N, n_mels)
+          mspec_egg, _ = self.get_mspec(self, x[2, :], fs) # (N, n_mels)
         elif pth.suffix == '.pt': mspec = torch.load(str(pth)) # (N, n_mels), no additional transforms
         else: print('file type not supported')
         
@@ -83,7 +86,7 @@ class AutoVCDataset(data.Dataset):
         assert mspec_src.shape[0] == len(rmse)
 
         # random crop everything in the same way
-        mspec_src, mspec_tegg, mspec_egg, f0, rmse = self.random_crop(mspec_src, mspec_tegg, mspec_egg, torch.Tensor(f0), torch.Tensor(rmse))
+        mspec_src, mspec_tegg, mspec_egg, f0, rmse = self.random_crop(mspec_src, mspec_tegg, mspec_egg, f0, torch.Tensor(rmse))
 
         #one-hot encode conditioning vars
         f0_1hot, f0_i = self.quantize_f0_numpy(f0.detach().numpy())
@@ -97,6 +100,10 @@ class AutoVCDataset(data.Dataset):
 
         return mspec_src, mspec_tegg, mspec_egg, spk_emb, (f0_1hot, f0_i), (rmse_1hot,rmse_i)
 
+    def get_mspec(self, x, fs):
+        m, x = get_mspec_from_array(x, input_sr=fs, is_hifigan=True, return_waveform=True) # (N, n_mels)
+        return m, x
+    
     def apply_transforms(self, x, fs, transforms=None):
       # TO-DO directly on .wav. (ALL CHANNELS!)
       # scale / stretch / shrink by minor factor
@@ -126,6 +133,13 @@ class AutoVCDataset(data.Dataset):
             rmse = rmse[crop_start:crop_start+clen]
         return mspec_src, mspec_tegg, mspec_egg, f0, rmse
 
+    def f0_cent_normalization(self, x):
+        c = np.log2(x+1e-6) - self.min_c
+        index_nonzero = (c > 0)
+        c[index_nonzero] = c[index_nonzero]/self.max_c
+        c[index_nonzero] = np.clip(c[index_nonzero], 0, 1)
+        return c
+    
     def f0_normalization(self, f0):
         f0 = np.log(f0.astype(float).copy())
         index_nonzero = (f0 > 0)
@@ -158,7 +172,7 @@ class AutoVCDataset(data.Dataset):
         enc[np.arange(len(x)), x.astype(np.int32)] = 1.0
         return enc, x.astype(np.int64)
 
-    def quantize_one_hot_torch(x, num_bins=256):
+    def quantize_f0_torch(x, num_bins=256):
         # x is normalized to be between 0 and 1
         B = x.size(0)
         x = x.view(-1).clone()

model_vc.py
@@ -253,8 +253,8 @@ class Generator(nn.Module):
         
         decoder_outputs = self.decoder(encoder_outputs) # predicted EGG
      
-        mel_outputs_vtnet = self.vtnet(decoder_outputs.transpose(2,1), c_org, amp_src)
-        mel_outputs_vtnet = decoder_outputs + mel_outputs_vtnet.transpose(2,1)
+        mel_outputs_vtnet = self.vtnet(decoder_outputs.transpose(2,1), c_org, amp_src) 
+        mel_outputs_vtnet = decoder_outputs + mel_outputs_vtnet.transpose(2,1) #highway nextwork --> aims for vtnet output to be the spectrogram "mask/filter" of the EGG
 
         mel_outputs_postnet = self.postnet(mel_outputs_vtnet.transpose(2, 1))
         mel_outputs_postnet = mel_outputs_vtnet + mel_outputs_postnet.transpose(2, 1)

plot_utils.py
@@ -9,9 +9,11 @@ import io
 
 
 def plot_spec(mspecs, title_str, out_path):
+
+  #incoming tuple: x_src[0], x_egg[0], x_pred_egg[0], x_tegg[0], x_pred_tegg[0], x_pred_psnt[0]
   
   mspecs = [np.transpose(np.squeeze(m.to('cpu').detach().numpy())) for m in mspecs]
-  (speech, EGG, tEGG, pred_EGG, pred_tEGG, postnet) = tuple(mspecs)
+  (speech, EGG, pred_EGG, tEGG, pred_tEGG, postnet) = tuple(mspecs)
 
   fig, [[ax1, ax2],[ax3, ax4], [ax5, ax6]] = plt.subplots(nrows=3, ncols=2, sharex=True, sharey=True, figsize=(16,16))
 
@@ -19,25 +21,26 @@ def plot_spec(mspecs, title_str, out_path):
   ax1.set(title='Speech')
   fig.colorbar(img, ax=ax1, format="%+2.f dB")
 
-  img = librosa.display.specshow(EGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax2)
-  ax2.set(title='EGG')
+  img = librosa.display.specshow(postnet, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax2)
+  ax2.set(title='PostNet')
   fig.colorbar(img, ax=ax2, format="%+2.f dB")
 
-  img = librosa.display.specshow(tEGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax3)
-  ax3.set(title='tEGG')
+  img = librosa.display.specshow(EGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax3)
+  ax3.set(title='EGG')
   fig.colorbar(img, ax=ax3, format="%+2.f dB")
 
   img = librosa.display.specshow(pred_EGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax4)
   ax4.set(title='EGG Prediction')
   fig.colorbar(img, ax=ax4, format="%+2.f dB")
-  
-  img = librosa.display.specshow(pred_tEGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax5)
-  ax5.set(title='tEGG Prediction')
-  fig.colorbar(img, ax=ax5, format="%+2.f dB")
 
-  img = librosa.display.specshow(postnet, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax6)
-  ax6.set(title='PostNet')
+  img = librosa.display.specshow(tEGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax5)
+  ax5.set(title='tEGG')
+  fig.colorbar(img, ax=ax5, format="%+2.f dB")
+  
+  img = librosa.display.specshow(pred_tEGG, hop_length=hp.hop_length, x_axis='time', y_axis='mel', ax=ax6)
+  ax6.set(title='tEGG Prediction')
   fig.colorbar(img, ax=ax6, format="%+2.f dB")
+
   
   fig.suptitle(title_str)
   savepath = os.path.join(out_path, title_str + '.png')
@@ -48,9 +51,16 @@ def plot_spec(mspecs, title_str, out_path):
 
 def spec_to_tensorboard(mspecs, title_str, out_path):
   fig = plot_spec(mspecs, title_str, out_path)
+  # Draw figure on canvas
   fig.canvas.draw()
+  # Convert the figure to numpy array, read the pixel values and reshape the array
   image = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+  
   image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+  # Normalize into 0-1 range for TensorBoard(X).
   image = image / 255.0
-  image = np.swapaxes(image, 0, 2)
+  # image = np.fliplr(image) #flip image LR
+  image = image.swapaxes(0, 1) #transpose
+  image = image.swapaxes(0, 2) #put color channel first
+
   return image

train.py
@@ -93,7 +93,7 @@ def train(args):
 
     if args.vocode:
       print("[MODEL] Setting up neural vocoder.")
-      vocode = Vocoder(out_path=hp.output_path, writer=writer)
+      vocoder = Vocoder(out_path=hp.output_path, writer=writer)
 
     print("[TRAIN] Beginning training")
     start_time = time.time()
@@ -181,7 +181,7 @@ def train(args):
                 image = spec_to_tensorboard( (x_src[0], x_egg[0], x_pred_egg[0], x_tegg[0], x_pred_tegg[0], x_pred_psnt[0]), f'E{epoch}', out_path)
                 writer.add_image(f'G/MelSpec_E{epoch}_I{iter}', image, epoch)    
                 if args.vocode:
-                    vocode(x_src, x_pred_psnt, epoch, i)
+                    vocoder.vocode(x_src, x_pred_psnt, epoch, i)
                     
             iter += 1
             if iter >= hp.n_iters:
@@ -226,7 +226,7 @@ def train(args):
             image = spec_to_tensorboard( (x_src[0], x_egg[0], x_pred_egg[0], x_tegg[0], x_pred_tegg[0], x_pred_psnt[0]), f'E{epoch}_v{i}', out_path)
             writer.add_image(f'valid/MelSpec_E{epoch}_{i}', image, epoch)
             if args.vocode:
-                vocode(x_src, x_pred_psnt, epoch, i, valid=True)
+                vocoder.vocode(x_src, x_pred_psnt, epoch, i, valid=True)
 
         valid_losses = {k: np.mean(valid_losses[k]) for k in valid_losses.keys()}
         for tag in valid_losses.keys(): writer.add_scalar('valid/' + tag, valid_losses[tag], iter)